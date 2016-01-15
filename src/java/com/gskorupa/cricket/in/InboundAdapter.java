020d84ba0a1517e952d817225dde1f7f73fa84ff
greg <g.skorupa@gmail.com>
2016-01-16 00:55:53 +0100

    issue 50, 51, 52

--- src/java/com/gskorupa/cricket/in/InboundAdapter.java
+++ src/java/com/gskorupa/cricket/in/InboundAdapter.java
@@ -72,4 +72,7 @@ public class InboundAdapter {
         }
     }
 
+    public void destroy(){
+        
+    }
 }
--- src/java/com/gskorupa/cricket/out/OutboundAdapter.java
+++ src/java/com/gskorupa/cricket/out/OutboundAdapter.java
@@ -71,5 +71,9 @@ public class OutboundAdapter {
             e.printStackTrace();
         }
     }
+    
+    public void destroy(){
+        
+    }
 
 }
